5af3259521fcc687f4855668316070e0b0e5f16d
rickherrera3001 <rihr.3001@gmail.com>
2024-04-16 14:28:12 -0600

    firts commit

---
+++ BrillioIOSCodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrillioIOSCodingTestApp.swift
+//  BrillioIOSCodingTest
+//
+//  Created by Ricardo Developer on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BrillioIOSCodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
